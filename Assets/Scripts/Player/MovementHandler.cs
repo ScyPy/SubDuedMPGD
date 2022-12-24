fcafce449f5e8d9ae94915861e6e9ed4a81b50e7
ScyPy <shamari.1999@outlook.com>
2022-12-24 22:31:28 +0300

    environment & controller updates

---
+++ Assets/Scripts/Player/MovementHandler.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using Cinemachine;
+public class MovementHandler : MonoBehaviour
+{
+    [SerializeField]
+    private InputManager _inputManager;
+    private CharacterController _characterController;
+    [SerializeField]
+    private Camera _mainCamera;
+
+    private float _mouseSensitivity = 25f;
+    private float _speed = 5f;
+    private const float _gravity = -9.81f;
+    private float _velocity;
+    private float _pitch;
+    private float _yaw;
+    private float _xRot = 0f;
+    private const float _rotationSpeed = 60f;
+    
+    void Start()
+    {
+        _inputManager = GetComponent<InputManager>();
+        _characterController = GetComponent<CharacterController>();
+        _mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gravity();
+        Look();
+        MovePlayer();
+    }
+
+    private void Look()
+    {
+        //_yaw += _inputManager.MouseLook.x * Time.deltaTime * _mouseSensitivity;
+        ////_pitch += -_inputManager.MouseLook.y * Time.deltaTime * _mouseSensitivity;
+        //////clamp to limit angle within 360 degrees
+        //_yaw = ClampAngle(_yaw, float.MinValue, float.MaxValue);
+        ////_pitch = ClampAngle(_pitch, -90, 90);
+        //transform.rotation = Quaternion.SlerpUnclamped(transform.rotation, Quaternion.Euler(0.0f, _yaw, 0.0f), _rotationSpeed * Time.deltaTime);
+        ////transform.Rotate(Vector3.up, _inputManager.MouseLook.x * Time.deltaTime);
+        //_xRot -= _inputManager.MouseLook.y * Time.deltaTime * _mouseSensitivity;
+        //_xRot = Mathf.Clamp(_xRot, -90, 90);
+        //Vector3 targetRotation = transform.eulerAngles;
+        //targetRotation.x = _xRot;
+        //_mainCamera.transform.eulerAngles = targetRotation;
+        transform.rotation = Quaternion.Euler(0f, _mainCamera.transform.eulerAngles.y, 0f);
+
+    }
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
+    }
+
+    private void Gravity()
+    {
+        if (_characterController.isGrounded && _velocity < 0)
+        {
+            _velocity = -2f;
+        }
+
+        _velocity += _gravity * Time.deltaTime;
+    }
+    private void MovePlayer()
+    {
+        Vector3 moveDir = (_inputManager.MoveDirection.y * transform.forward) + (_inputManager.MoveDirection.x * transform.right);
+        _characterController.Move(moveDir * _speed * Time.deltaTime + new Vector3(0f, _velocity,0f) * Time.deltaTime);
+    }
+}
